9d5f437cb5a0bdd2cd5e26b62c0bffa5aceb9b1e
liweifan <lwf474686@ly.com>
2021-02-20 10:42:33 +0800

    name refactor

--- springcloud-ribbon/springcloud-ribbon-consumer/src/main/java/com/pancm/RibbonConsumerApplication.java
+++ springcloud-ribbon/springcloud-ribbon-consumer/src/main/java/com/pancm/RibbonConsumerApplication.java
@@ -18,9 +18,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class Application {
+public class RibbonConsumerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(RibbonConsumerApplication.class, args);
 		  System.out.println("第一个消费者服务启动...");
 	}
 }
--- springcloud-ribbon/springcloud-ribbon-eureka/src/main/java/com/pancm/RibbonEurekaApplication.java
+++ springcloud-ribbon/springcloud-ribbon-eureka/src/main/java/com/pancm/RibbonEurekaApplication.java
@@ -16,9 +16,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class RibbonEurekaApplication {
   public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
+      SpringApplication.run(RibbonEurekaApplication.class, args);
       System.out.println("注册中心服务启动...");
   }
 }
